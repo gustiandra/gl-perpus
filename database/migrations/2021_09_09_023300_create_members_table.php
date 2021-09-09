4d3f93c6a0abb6a4cbb800facf32be8232e3ca79
gustiandra <gustiandra49@student.untan.ac.id>
2021-09-09 09:39:16 +0700

    Migrations User & Member

---
+++ database/migrations/2021_09_09_023300_create_members_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('no_hp');
+            $table->string('address');
+            $table->string('job');
+            $table->string('photo_IdCard');
+            $table->string('photo');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
