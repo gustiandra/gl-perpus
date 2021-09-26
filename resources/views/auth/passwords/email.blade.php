e47c33926a50b647b0f8eab0a7ccaec5b92c7dbd
gustiandra <gustiandra49@student.untan.ac.id>
2021-09-26 18:25:37 +0700

    View Reset & Email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,39 @@
-@extends('layouts.app')
-
+@extends('auth.layouts.auth', ['title' => 'Reset Passowrd'])
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+    <div class="container">
+        <div class="row align-items-center justify-content-center mt-3">
+            <div class="col-md-8">
+                <div class="mb-4">
+                    <a href="{{ route('home') }}" class="text-decoration-none"><h1 class="font-weight-bold text-primary">GL-Perpus</h1></a>
                 </div>
+                <div class="mb-4">
+                    <h4 class="font-weight-bolder">Reset Password</h4>
+                    {{-- <p class="mb-4">Membaca adalah jembatan ilmu</p> --}}
+                </div>
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="username">Email</label>
+                        <input type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>                                    
+                    <input type="submit" value="Kirim Link Pembaharuan Password" class="btn btn-block btn-primary">
+
+                </form>                
+                <span class="d-flex justify-content-center mt-5"><a class="forgot-pass" href="{{ route('login') }}">
+                    Login
+                </a></span>
             </div>
         </div>
     </div>
-</div>
 @endsection
+
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,3 +1,49 @@
+@extends('auth.layouts.auth', ['title' => 'Lupa Password'])
+@section('content')
+    <div class="container">
+        <div class="row align-items-center justify-content-center mt-3">
+            <div class="col-md-8">
+                <div class="mb-4">
+                    <a href="{{ route('home') }}"><h1 class="font-weight-bold text-primary">GL-Perpus</h1></a>
+                </div>
+                <div class="mb-4">
+                    <h4 class="font-weight-bolder">Lupa Password?</h4>
+                    {{-- <p class="mb-4">Membaca adalah jembatan ilmu</p> --}}
+                </div>                
+                <form method="POST" action="{{ route('password.update') }}">
+                    @csrf
+                    <input type="hidden" name="token" value="{{ $token }}">
+                    <div class="form-group">
+                        <label for="username">Email</label>
+                        <input type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="form-group ">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="form-group ">
+                        <label for="password-confirm">Password</label>
+                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                    </div>
+                    <input type="submit" value="Log In" class="btn btn-block btn-primary">
+
+                </form>                
+            </div>
+        </div>
+    </div>
+@endsection
+
+
 @extends('layouts.app')
 
 @section('content')
