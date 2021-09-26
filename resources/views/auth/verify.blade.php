8bc4e5afa9192ba133e4eff4969c6b19ebad91d4
gustiandra <gustiandra49@student.untan.ac.id>
2021-09-26 18:10:56 +0700

    View Verify Email

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,27 @@
-@extends('layouts.app')
-
+@extends('auth.layouts.auth', ['title' => 'Verifikasi Email'])
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
+    <div class="container">
+        <div class="row align-items-center justify-content-center mt-3">
+            <div class="col-md-8">
+                <div class="mb-4">
+                    <h1 class="font-weight-bold text-primary">GL-Perpus</h1>
                 </div>
+                <div class="mb-4">
+                    <h4 class="font-weight-bolder">Verifikasi Email</h4>
+                    {{-- <p class="mb-4">Membaca adalah jembatan ilmu</p> --}}
+                </div>                                
+                @if (session('resent'))
+                    <div class="alert alert-success" role="alert">
+                        {{ __('A fresh verification link has been sent to your email address.') }}
+                    </div>
+                @endif                
+                {{ __('Cek email anda untuk melakukan verifikasi.') }}
+                {{ __('Jika Anda tidak menerima email') }},
+                <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                    @csrf
+                    <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('klik untuk mengirim ulang') }}</button>.
+                </form>
             </div>
         </div>
     </div>
-</div>
 @endsection
